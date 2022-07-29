aff1e36f9383cb214f391c92d9bf91032e19b47f
rogerpinto22 <roger.pinto_may@outlook.com>
2022-07-28 22:49:00 -0500

    otro

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // importaciones
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cors = require('cors');
 
 require('dotenv').config();
 
